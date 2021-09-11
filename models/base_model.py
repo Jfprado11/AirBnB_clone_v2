1ecd93a724d89150b59398ae6f1e321b4d6a7c29
Felipe Prado <jfpc11@misena.edu.co>
2021-09-11 16:25:58 -0500

    fixing the output needed

--- models/base_model.py
+++ models/base_model.py
@@ -45,8 +45,10 @@ class BaseModel:
 
     def __str__(self):
         """Returns a string representation of the instance"""
+        new_dict = self.__dict__.copy()
+        del new_dict["_sa_instance_state"]
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        return '[{}] ({}) {}'.format(cls, self.id, new_dict)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
