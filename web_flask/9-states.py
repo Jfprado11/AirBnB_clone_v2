250d45390b159bfe5d3b224a525df0b6bf2608a6
Felipe Prado <jfpc11@misena.edu.co>
2021-09-12 17:54:47 -0500

    adding the state

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -13,13 +13,17 @@ app = Flask(__name__)
 
 
 @app.route("/states", strict_slashes=False)
+def states():
+    """show the states"""
+    states = storage.all(State)
+    return render_template("9-states.html", states=states)
+
+
 @app.route("/states/<id>", strict_slashes=False)
-def states_id(id=None):
+def states_id(id):
     """show the specific state with its cities"""
     states = storage.all(State)
-    if id is None:
-        return render_template("9-states.html", states=states)
-    elif any(id == value[6:] for value in states.keys()):
+    if any(id == value[6:] for value in states.keys()):
         key = "State." + id
         state = states[key]
         cities = storage.all(City)
